36128e98d50ff2b27ec4e53b5fdae4b2c2e7d649
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2021-01-20 11:34:52 +0100

    appli meteo en jQuery

--- app.JS/script.js
+++ app.JS/script.js
@@ -1,43 +1,40 @@
 // mes variables
 
-let result = document.getElementById("result");
-let ciel = document.getElementById("ciel");
-let ville = document.getElementById("input");
-let bouton = document.getElementById("valid");
+let result = $("#result");
+let ciel = $("#ciel");
+let button = $("#valid");
 let xhr = new XMLHttpRequest();
 
 //recuperation de l'API est affichage des demandes
 
-bouton.addEventListener("click", function (){
-    if(ville.value !== ""){
-        let requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+ville.value+"&lang=fr&units=metric&appid=9e873a431320b9766c279a6d50206ea9";
-        xhr.open("GET", requestUrl);
-        xhr.responseType = "json";
-        xhr.send();
-        xhr.onload = function (){
-            if(xhr.status === 200){
-                let response = xhr.response;
-                result.style.fontSize = "x-large";
-                result.innerHTML =
-                    "actuellement a " + ville.value + "<br>" + "<br>" +
-                    " lat " + response.coord.lat + "<br>" +
-                    " long " + response.coord.lon + "<br>" +
-                    "temperature " + Math.ceil(response.main.feels_like) + " degres" + "<br>" +
-                    "ressentie " + Math.ceil(response.main.temp) + " degres" + "<br>" +
-                    "temp max " + Math.ceil(response.main.temp_max) + " degres" + "<br>" +
-                    "temp min " + Math.ceil(response.main.temp_min) + " degres" + "<br>" +
-                    "humidité " + response.main.humidity + "<br>" +
-                    "pression " + response.main.pressure;
+button.click(function(){
+    let ville = $("#input").val();
+    if(ville !== ""){
+        let requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+ville+"&lang=fr&units=metric&appid=9e873a431320b9766c279a6d50206ea9";
+        $.getJSON(requestUrl,function (response){
+            console.log(response);
+            result.css("fontSize","x-large");
+            result.html(
+                "actuellement a " + ville + "<br>" + "<br>" +
+                " lat " + response.coord.lat + "<br>" +
+                " long " + response.coord.lon + "<br>" +
+                "temperature " + Math.ceil(response.main.feels_like) + " degres" + "<br>" +
+                "ressentie " + Math.ceil(response.main.temp) + " degres" + "<br>" +
+                "temp max " + Math.ceil(response.main.temp_max) + " degres" + "<br>" +
+                "temp min " + Math.ceil(response.main.temp_min) + " degres" + "<br>" +
+                "humidité " + response.main.humidity + "<br>" +
+                "pression " + response.main.pressure
+            );
 
-                ciel.style.fontSize = "x-large";
-                let renduCiel = response.weather[0].description;
+            ciel.css("fontSize",  "x-large");
+            let renduCiel = response.weather[0].description;
 
-                afficheFond(renduCiel);
-                ciel.innerHTML =
-                    "vitesse du vent " + response.wind.speed + "<br>" +
-                    "ciel " + renduCiel
-            }
-        }
+            afficheFond(renduCiel);
+            ciel.html(
+                "vitesse du vent " + response.wind.speed + "<br>" +
+                "ciel " + renduCiel
+            )
+        });
     }
 })
 
@@ -46,31 +43,34 @@ bouton.addEventListener("click", function (){
 function afficheFond (renduCiel) {
     switch (renduCiel){
         case "partiellement nuageux":
-            ciel.style.backgroundImage = "url('./images/partiellementNuageux.jpg')";
+            ciel.css("backgroundImage", "url('./images/partiellementNuageux.jpg')");
             break
         case "nuageux":
-            ciel.style.backgroundImage = "url('./images/nuageux.jpg')";
+            ciel.css("backgroundImage", "url('./images/nuageux.jpg')");
             break
         case "peu nuageux":
-            ciel.style.backgroundImage = "url('./images/peuNuageux.jpg')";
+            ciel.css("backgroundImage", "url('./images/peuNuageux.jpg')");
             break
         case "brouillard":
-            ciel.style.backgroundImage = "url('./images/brouillard.jpg')";
+            ciel.css("backgroundImage", "url('./images/brouillard.jpg')");
             break
         case "ciel dégagé":
-            ciel.style.backgroundImage = "url('./images/cielDegage.jpg')";
+            ciel.css("backgroundImage", "url('./images/cielDegage.jpg')");
             break
         case "couvert":
-            ciel.style.backgroundImage = "url('./images/couvert.jpg')";
+            ciel.css("backgroundImage", "url('./images/couvert.jpg')");
             break
         case "pluie modérée":
-            ciel.style.backgroundImage = "url('./images/pluieModeree.jpg')";
+            ciel.css("backgroundImage", "url('./images/pluieModeree.jpg')");
+            break
+        case "légère pluie":
+            ciel.css("backgroundImage", "url('./images/legerePluie.jpg')");
             break
         case "légères chutes de neige":
-            ciel.style.backgroundImage = "url('./images/legeresChutesDeNeige.jpg')";
+            ciel.css("backgroundImage", "url('./images/legeresChutesDeNeige.jpg')");
             break
         case "chutes de neige":
-            ciel.style.backgroundImage = "url('./images/chutesDeNeige.jpg')";
+            ciel.css("backgroundImage", "url('./images/chutesDeNeige.jpg')");
             break
     }
 }
